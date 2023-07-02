2465ab277097a2a809326311eebde0b02b7d7b06
eymen efe altun <eymenefealtun@hotmail.com>
2023-07-02 17:02:57 +0300

    refactor: Duplicateion view refacrtored

--- Tourism.MainPage/MVVM/View/DuplicateOperationView.xaml.cs
+++ Tourism.MainPage/MVVM/View/DuplicateOperationView.xaml.cs
@@ -1,11 +1,10 @@
-﻿using Microsoft.IdentityModel.Tokens;
-using System;
-using System.Linq;
-using System.Windows;
-using System.Windows.Controls;
+﻿using System.Windows.Controls;
+using Tourism.Business.Abstract.Models;
 using Tourism.Business.DependencyResolvers.Ninject;
 using Tourism.DataAccess.Abstract;
 using Tourism.Entities.Concrete;
+using Tourism.Entities.Models;
+using System.Windows;
 
 namespace Tourism.MainPage.MVVM.View
 {
@@ -14,66 +13,102 @@ namespace Tourism.MainPage.MVVM.View
         private IMainCategoryService _mainCategoryService;
         private ISubCategoryService _subCategoryService;
         private IOperationService _operationService;
+        private IOperationMainService _operationMainService;
+        private TextBlock[] _textBlocks;
+        private DatePicker[] _startDates;
+        private DatePicker[] _endDates;
+        private TextBox[] _tboxDocumentCodes;
         public DuplicateOperationView()
         {
             InitializeComponent();
             _mainCategoryService = Instancefactory.GetInstance<IMainCategoryService>();
             _subCategoryService = Instancefactory.GetInstance<ISubCategoryService>();
             _operationService = Instancefactory.GetInstance<IOperationService>();
+            _operationMainService = Instancefactory.GetInstance<IOperationMainService>();
 
-            cboxMainCategory.ItemsSource = _mainCategoryService.GetAll();
+            dgwOperationMain.ItemsSource = _operationMainService.GetOperationMain();
+            TextBlock[] textBlocks = { tboxFirst, tboxSecond, tboxThird, tboxFourth, tboxFifth, tboxSixth, tboxSeventh, tboxEighth, tboxNinth, tboxTenth };
+            DatePicker[] startDates = { datePickFirstStartDate, datePickSecondStartDate, datePickThirdStartDate, datePickFourthStartDate, datePickFifthStartDate, datePickSixthStartDate, datePickSeventhStartDate, datePickEighthStartDate, datePickNinthStartDate, datePickTenthStartDate };
+            DatePicker[] endDates = { datePickFirstEndDate, datePicSecondkEndDate, datePickThirdEndDate, datePickFourthEndDate, datePickFifthEndDate, datePickSixthEndDate, datePickSeventhEndDate, datePickEighthEndDate, datePickNinthEndDate, datePickTenthEndDate };
+            TextBox[] tboxDocumentCodes = { tboxFirsDocumentCode, tboxSecondDocumentCode, tboxThirdDocumentCode, tboxFourthDocumentCode, tboxFifthDocumentCode, tboxSixthDocumentCode, tboxSeventhDocumentCode, tboxEighthDocumentCode, tboxNinthDocumentCode, tboxTenthDocumentCode };
+            _textBlocks = textBlocks;
+            _endDates = endDates;
+            _startDates = startDates;
+            _tboxDocumentCodes = tboxDocumentCodes;
 
         }
 
-        private void cboxMainCategory_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private int _chosenOperationId;
+        private string _documentCode;
+
+
+        private void btnChooseForDeuplication_Click(object sender, System.Windows.RoutedEventArgs e)
         {
-            if (cboxMainCategory.SelectedIndex > -1)
-            {
-                cboxSubCategory.IsHitTestVisible = true;
-                cboxSubCategory.ItemsSource = _subCategoryService.GetByMainCategory(Convert.ToInt32(cboxMainCategory.SelectedValue));
-            }
+            borderGrid.Visibility = Visibility.Collapsed;
+            Button button = sender as Button;
+            OperationMain operationMain = button.CommandParameter as OperationMain;
+
+            _chosenOperationId = operationMain.Id;
+            _documentCode = operationMain.DocumentCode;
+            tboxNumberOfDuplicate.Visibility = Visibility.Visible;
+            cboxNumberOfDuplicate.Visibility = Visibility.Visible;
         }
 
-        private void cboxSubCategory_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void btnRefresh_Click(object sender, RoutedEventArgs e)
         {
-            if (cboxSubCategory.SelectedIndex > -1)
-            {
-                cboxOperation.IsHitTestVisible = true;
-                cboxOperation.ItemsSource = _operationService.GetBySubCategory(Convert.ToInt32(cboxSubCategory.SelectedValue));
-            }
+            borderGrid.Visibility = Visibility.Visible;
+            dgwOperationMain.ItemsSource = _operationMainService.GetOperationMain();
+            tboxNumberOfDuplicate.Visibility = Visibility.Collapsed;
+            cboxNumberOfDuplicate.Visibility = Visibility.Collapsed;
         }
 
-        private void cboxOperation_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void tboxSearchOperations_TextChanged(object sender, TextChangedEventArgs e)
         {
-            if (cboxOperation.SelectedIndex > -1)
-            {
-                cboxNumberOfDuplicate.IsHitTestVisible = true;
-
-            }
-       
-
-            MessageBox.Show(cboxOperation.SelectedIndex.ToString());
-            cboxOperation.Text = "asdasdasdasdasdasdasdasds";
+            dgwOperationMain.ItemsSource = _operationMainService.GetByDocumentCode(tboxSearchOperations.Text);
 
         }
 
-        private void cboxOperation_TextChanged(object sender, TextChangedEventArgs e)
+        private void cboxNumberOfDuplicate_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            //MessageBox.Show(cboxOperation.Text);
+            if (cboxNumberOfDuplicate.SelectedIndex > -1)
+            {
+                tBlockEndDate.Visibility = Visibility.Visible;
+                tBlockStartDate.Visibility = Visibility.Visible;
+                tBlockDocumentCode.Visibility = Visibility.Visible;
 
-            if (cboxOperation.Text != String.Empty)
-            {
-                cboxOperation.ItemsSource = _operationService.GetBySubCategoryAndDocumentCode(cboxOperation.Text, Convert.ToInt32(cboxSubCategory.SelectedValue));
-            }
-            else if (cboxOperation.Text == String.Empty)
-            {
-                cboxOperation.ItemsSource = _operationService.GetBySubCategory(Convert.ToInt32(cboxSubCategory.SelectedValue));
+                for (int i = 0; i < cboxNumberOfDuplicate.SelectedIndex + 1; i++)
+                {
+                    
+                    _textBlocks[i].Visibility = Visibility.Visible;
+                    _endDates[i].Visibility = Visibility.Visible;
+                    _startDates[i].Visibility = Visibility.Visible;
+                    _tboxDocumentCodes[i].Visibility = Visibility.Visible;
+
+                }
+                for (int i = 9; i > cboxNumberOfDuplicate.SelectedIndex; i--)
+                {
+                    _textBlocks[i].Visibility = Visibility.Collapsed;
+                    _endDates[i].Visibility = Visibility.Collapsed;
+                    _startDates[i].Visibility = Visibility.Collapsed;
+                    _tboxDocumentCodes[i].Visibility = Visibility.Collapsed;
+                    _endDates[i].SelectedDate = null;
+                    _startDates[i].SelectedDate = null;
+                    _tboxDocumentCodes[i].Text = string.Empty;
+                }
             }
 
         }
+
+
+
+
+
 
     }
 
 
-
 }
+
+
+
+
