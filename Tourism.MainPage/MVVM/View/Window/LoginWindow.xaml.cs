a80841da22c71f956790c05bf099e22da9611776
eymen efe altun <eymenefealtun@hotmail.com>
2023-07-05 00:08:49 +0300

    style: added login window

---
+++ Tourism.MainPage/MVVM/View/Window/LoginWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+namespace Tourism.MainPage.MVVM.View.Window
+{
+    public partial class LoginWindow : System.Windows.Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
